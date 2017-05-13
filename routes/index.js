32ed38832da3dfe116489d6cd868627d8ddc0f93
Nisheet Sharma <nisheethsharma@gmail.com>
2017-05-13 20:40:44 +0530

    Done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var dir = require('../service.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(__dirname+'../views/home.html');
+  console.log(__dirname);
+  console.log(dir);
+  res.sendFile(dir+'/views/home.html');
 });
 
 module.exports = router;
